0e986285d2a8d17da790f7331837b98f68f3ceac
chase-west <chasetwest@gmail.com>
2025-10-24 14:20:01 -0700

    added build

--- build.js
+++ build.js
@@ -1,5 +1,12 @@
 import { execSync } from 'child_process';
+import { join } from 'path';
 
-// Change to frontend directory and run build
+// Change to frontend directory
 process.chdir('frontend');
+
+// Add node_modules/.bin to PATH
+const binPath = join(process.cwd(), 'node_modules', '.bin');
+process.env.PATH = `${binPath}:${process.env.PATH}`;
+
+// Run build
 execSync('npm run build', { stdio: 'inherit' });
